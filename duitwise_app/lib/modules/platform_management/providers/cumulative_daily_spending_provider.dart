aa53749d90821a48fc27b9a6b0ec0e261a222254
WanAdamm <wanadamworkmail@gmail.com>
2025-12-22 08:25:24 +0800

    reformat cumulative daily spneding provider.dart

--- duitwise_app/lib/modules/platform_management/providers/cumulative_daily_spending_provider.dart
+++ duitwise_app/lib/modules/platform_management/providers/cumulative_daily_spending_provider.dart
@@ -2,8 +2,10 @@ import 'package:duitwise_app/modules/financial_tracking/providers/transaction_pr
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final cumulativeDailySpendingProvider =
-    Provider.family<List<FlSpot>, String>((ref, uid) {
+final cumulativeDailySpendingProvider = Provider.family<List<FlSpot>, String>((
+  ref,
+  uid,
+) {
   final txAsync = ref.watch(transactionsStreamProvider(uid));
 
   return txAsync.when(
@@ -11,13 +13,10 @@ final cumulativeDailySpendingProvider =
       final Map<DateTime, double> dailyTotals = {};
 
       for (final tx in txs) {
-        final dt = DateTime.fromMillisecondsSinceEpoch(
-          tx.createdAt.toInt(),
-        );
+        final dt = DateTime.fromMillisecondsSinceEpoch(tx.createdAt.toInt());
         final dayKey = DateTime(dt.year, dt.month, dt.day);
 
-        dailyTotals[dayKey] =
-            (dailyTotals[dayKey] ?? 0) + tx.amount;
+        dailyTotals[dayKey] = (dailyTotals[dayKey] ?? 0) + tx.amount;
       }
 
       final days = dailyTotals.keys.toList()..sort();
